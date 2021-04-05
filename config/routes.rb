b2eaaaf2de26a9ab1a030613ee7cb25a82469e65
philippe <p.hilippebiau.t@gmail.com>
2021-04-05 19:43:53 +0200

    Add admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   resources :ideas
+
+    namespace :admin do
+    # Add dashboard for your models here
+    resources :ideas
+
+    	root to: "ideas#index" # <--- Root route
+  	end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
